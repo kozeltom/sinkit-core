87b4f22854a9dee169b1b86669f809fc936bb2ea
Bambula <kozelto@gmail.com>
2015-11-22 00:04:11 -0800

    circle-wildfly.sh

--- circle-wildfly.sh
+++ circle-wildfly.sh
@@ -3,8 +3,7 @@
 # @author Tomas Kozel
 
 export SINKIT_HOME=$PWD
-mkdir wildfly && cd wildfly
 git clone https://github.com/wildfly/wildfly.git
-cd wildfly-${WILDFLY_VERSION} && mvn install -DskipTests
+cd wildfly && mvn install -DskipTests
 cp build/target/wildfly-${WILDFLY_VERSION} ${SINKIT_HOME}/
 cp ${SINKIT_HOME}/standalone-ha.xml ${SINKIT_HOME}/wildfly-${WILDFLY_VERSION}/standalone/configuration/standalone-ha.xml
