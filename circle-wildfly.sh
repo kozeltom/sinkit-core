f8f9d64b5fb269cfa945443b70a33ed4b591f5dd
Bambula <kozelto@gmail.com>
2015-11-22 00:01:22 -0800

    circle-wildfly.sh

--- circle-wildfly.sh
+++ circle-wildfly.sh
@@ -2,6 +2,9 @@
 
 # @author Tomas Kozel
 
-cd ~
-wget http://download.jboss.org/wildfly/${WILDFLY_VERSION}/wildfly-${WILDFLY_VERSION}.zip
-unzip wildfly-${WILDFLY_VERSION}.zip && cp standalone-ha.xml ${JBOSS_HOME}/standalone/configuration/standalone-ha.xml
+export SINKIT_HOME=$PWD
+mkdir wildfly && cd wildfly
+git clone https://github.com/wildfly/wildfly.git
+cd wildfly-${WILDFLY_VERSION} && mvn install -DskipTests
+cp build/target/wildfly-${WILDFLY_VERSION} ${SINKIT_HOME}/
+cp ${SINKIT_HOME}/standalone-ha.xml ${SINKIT_HOME}/wildfly-${WILDFLY_VERSION}/standalone/configuration/standalone-ha.xml
